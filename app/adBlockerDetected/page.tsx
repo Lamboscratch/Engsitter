d2d0f391e4ead17e84c575f9f2bac32c2c1e5408
Mattia <mattialamberti95@libero.it>
2024-04-19 20:51:34 +0200

    Fixing adBlockerDetector page style

--- app/adBlockerDetected/page.tsx
+++ app/adBlockerDetected/page.tsx
@@ -4,16 +4,14 @@ import ContainerApp from "../Components/ContainerApp";
 const page = () => {
     return (
         <ContainerApp>
-            <ContainerApp style="my-16">
-                <Flex direction="column" align="center">
-                    <Heading as="h1" size="8">
-                        AdBlocker Detected
-                    </Heading>
-                    <Text className="pt-6 max-w-prose text-center" size="4" as="p">
-                        It appears that you are using an ad-blocking extension in your browser. Please disable it and reload the page.
-                    </Text>
-                </Flex>
-            </ContainerApp>
+            <Flex className="notFoundHeight" direction="column" align="center" justify="center">
+                <Heading as="h1" size="8">
+                    AdBlocker Detected
+                </Heading>
+                <Text className="pt-6 max-w-prose text-center" size="4" as="p">
+                    It appears that you are using an ad-blocking extension in your browser. Please disable it and reload the page.
+                </Text>
+            </Flex>
         </ContainerApp>
     );
 };
