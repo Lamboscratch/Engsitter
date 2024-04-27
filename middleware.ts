2a8cc14fb3a7fc3815482724ff07914f07909b99
Mattia <mattialamberti95@libero.it>
2024-04-27 13:43:25 +0200

    Setting different CSP headers for development and production

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ export function middleware(request: NextRequest) {
     const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
     const cspHeader = `
         default-src 'self' https://www3.doubleclick.net/ https://marketingplatform.google.com/about/enterprise/;
-        script-src 'self' 'nonce-${nonce}' 'sha256-eMuh8xiwcX72rRYNAGENurQBAcH7kLlAUQcoOri3BIo=' 'sha256-10/0FESKC8299B5TCE+dGDo0JdZcRdny7kTczowOIYE=' giscus.app analytics.umami.is;
+        script-src 'self' ${process.env.NODE_ENV === "production" ? "" : "'unsafe-eval'"} 'nonce-${nonce}' 'sha256-eMuh8xiwcX72rRYNAGENurQBAcH7kLlAUQcoOri3BIo=' 'sha256-10/0FESKC8299B5TCE+dGDo0JdZcRdny7kTczowOIYE=' giscus.app analytics.umami.is;
         style-src 'self' 'sha256-jUf+GtHxjTTTtqEHmSnVBpcHE67W4pMA+IZfBvqzwYo=';
         font-src 'self';
         form-action 'self';
