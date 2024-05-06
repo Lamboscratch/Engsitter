528ed2fc0023cfd64c8b812aa9c432bee1a5367d
Mattia <mattialamberti95@libero.it>
2024-05-06 23:11:32 +0200

    Updating sitemap.xml route with better information

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,11 +2,6 @@ import siteMetadata from "@/app/data/siteMetadata";
 import { allPosts } from "contentlayer/generated";
 import { MetadataRoute } from "next";
 
-interface SitemapType {
-    url: string;
-    lastModified: string;
-}
-
 export default function sitemap(): MetadataRoute.Sitemap {
     const siteUrl = siteMetadata.siteUrl;
 
@@ -15,20 +10,26 @@ export default function sitemap(): MetadataRoute.Sitemap {
         .map((post) => ({
             url: `${siteUrl}/posts/${post.slug}`,
             lastModified: post.lastmod || post.date.split("T")[0],
+            changeFrequency: "monthly",
+            priority: 0.5,
         }));
 
     const courses = new Set(postRoutes.map((post) => post.url.split("/")[4]));
-    let coursesRoutes: SitemapType[] = [];
+    let coursesRoutes: any[] = [];
     courses.forEach((course) => {
         coursesRoutes.push({
             url: `${siteUrl}/posts/${course}`,
             lastModified: new Date().toISOString().split("T")[0],
+            changeFrequency: "daily",
+            priority: 0.7,
         });
     });
 
     const routes = ["", "posts", "resources"].map((route) => ({
         url: `${siteUrl}/${route}`,
         lastModified: new Date().toISOString().split("T")[0],
+        changeFrequency: "hourly",
+        priority: 0.9,
     }));
 
     return [...routes, ...coursesRoutes, ...postRoutes];
