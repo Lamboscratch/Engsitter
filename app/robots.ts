7701842b195cc61fdd9e8805bd1699552b0ff26a
Mattia <mattialamberti95@libero.it>
2024-05-06 20:42:01 +0200

    Updating robots.txt with better information

--- app/robots.ts
+++ app/robots.ts
@@ -1,10 +1,27 @@
+import siteMetadata from "@/app/data/siteMetadata";
 import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
     return {
-        rules: {
-            userAgent: "*",
-            allow: "/",
-        },
+        rules: [
+            {
+                userAgent: "Quantcastbot",
+                disallow: "",
+                crawlDelay: 1,
+            },
+            {
+                userAgent: "GPTBot",
+                disallow: "/",
+            },
+            {
+                userAgent: "ChatGPT-User",
+                disallow: "/",
+            },
+            {
+                userAgent: "*",
+                disallow: "",
+            },
+        ],
+        sitemap: `${siteMetadata.siteUrl}/sitemap.xml`,
     };
 }
